fe9204b4b177d122bdc8e1cbe99f21cecf1b110e
Nightwarri0R <37656830+Nightwarri0R@users.noreply.github.com>
2019-02-08 14:52:31 +0000

    I fixed  a small error in the display

--- Concentration/Concentration/MenuForm.Designer.cs
+++ Concentration/Concentration/MenuForm.Designer.cs
@@ -50,11 +50,12 @@
             // 
             this.lConcentration.AutoSize = true;
             this.lConcentration.BackColor = System.Drawing.Color.Transparent;
-            this.lConcentration.Font = new System.Drawing.Font("Pristina", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lConcentration.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lConcentration.ForeColor = System.Drawing.Color.White;
-            this.lConcentration.Location = new System.Drawing.Point(66, 32);
+            this.lConcentration.Location = new System.Drawing.Point(88, 39);
+            this.lConcentration.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lConcentration.Name = "lConcentration";
-            this.lConcentration.Size = new System.Drawing.Size(210, 52);
+            this.lConcentration.Size = new System.Drawing.Size(351, 58);
             this.lConcentration.TabIndex = 0;
             this.lConcentration.Text = "Concentration";
             // 
@@ -62,11 +63,12 @@
             // 
             this.btnScores.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnScores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnScores.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnScores.ForeColor = System.Drawing.Color.White;
-            this.btnScores.Location = new System.Drawing.Point(90, 98);
+            this.btnScores.Location = new System.Drawing.Point(120, 121);
+            this.btnScores.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnScores.Name = "btnScores";
-            this.btnScores.Size = new System.Drawing.Size(146, 39);
+            this.btnScores.Size = new System.Drawing.Size(195, 48);
             this.btnScores.TabIndex = 1;
             this.btnScores.Text = "View Highscores";
             this.btnScores.UseVisualStyleBackColor = false;
@@ -76,11 +78,12 @@
             // 
             this.btn1Player.BackColor = System.Drawing.Color.DodgerBlue;
             this.btn1Player.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn1Player.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1Player.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1Player.ForeColor = System.Drawing.Color.White;
-            this.btn1Player.Location = new System.Drawing.Point(90, 146);
+            this.btn1Player.Location = new System.Drawing.Point(120, 180);
+            this.btn1Player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn1Player.Name = "btn1Player";
-            this.btn1Player.Size = new System.Drawing.Size(146, 39);
+            this.btn1Player.Size = new System.Drawing.Size(195, 48);
             this.btn1Player.TabIndex = 2;
             this.btn1Player.Text = "1 Player";
             this.btn1Player.UseVisualStyleBackColor = false;
@@ -90,11 +93,12 @@
             // 
             this.btn2Player.BackColor = System.Drawing.Color.DodgerBlue;
             this.btn2Player.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn2Player.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2Player.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2Player.ForeColor = System.Drawing.Color.White;
-            this.btn2Player.Location = new System.Drawing.Point(90, 191);
+            this.btn2Player.Location = new System.Drawing.Point(120, 235);
+            this.btn2Player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn2Player.Name = "btn2Player";
-            this.btn2Player.Size = new System.Drawing.Size(146, 39);
+            this.btn2Player.Size = new System.Drawing.Size(195, 48);
             this.btn2Player.TabIndex = 3;
             this.btn2Player.Text = "2 Player";
             this.btn2Player.UseVisualStyleBackColor = false;
@@ -104,11 +108,12 @@
             // 
             this.btnEasy.BackColor = System.Drawing.Color.SpringGreen;
             this.btnEasy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEasy.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEasy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEasy.ForeColor = System.Drawing.Color.White;
-            this.btnEasy.Location = new System.Drawing.Point(90, 234);
+            this.btnEasy.Location = new System.Drawing.Point(120, 288);
+            this.btnEasy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEasy.Name = "btnEasy";
-            this.btnEasy.Size = new System.Drawing.Size(146, 39);
+            this.btnEasy.Size = new System.Drawing.Size(195, 48);
             this.btnEasy.TabIndex = 4;
             this.btnEasy.Text = "Easy Mode";
             this.btnEasy.UseVisualStyleBackColor = false;
@@ -118,11 +123,12 @@
             // 
             this.btnHard.BackColor = System.Drawing.Color.Crimson;
             this.btnHard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHard.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHard.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHard.ForeColor = System.Drawing.Color.White;
-            this.btnHard.Location = new System.Drawing.Point(90, 279);
+            this.btnHard.Location = new System.Drawing.Point(120, 343);
+            this.btnHard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnHard.Name = "btnHard";
-            this.btnHard.Size = new System.Drawing.Size(146, 39);
+            this.btnHard.Size = new System.Drawing.Size(195, 48);
             this.btnHard.TabIndex = 5;
             this.btnHard.Text = "Hard Mode";
             this.btnHard.UseVisualStyleBackColor = false;
@@ -137,20 +143,22 @@
             this.panelPlayer.Controls.Add(this.lPlayer2);
             this.panelPlayer.Controls.Add(this.lPlayer1);
             this.panelPlayer.Controls.Add(this.lEnterName);
-            this.panelPlayer.Font = new System.Drawing.Font("Pristina", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panelPlayer.ForeColor = System.Drawing.Color.White;
-            this.panelPlayer.Location = new System.Drawing.Point(32, 87);
+            this.panelPlayer.Location = new System.Drawing.Point(43, 107);
+            this.panelPlayer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelPlayer.Name = "panelPlayer";
-            this.panelPlayer.Size = new System.Drawing.Size(300, 248);
+            this.panelPlayer.Size = new System.Drawing.Size(450, 305);
             this.panelPlayer.TabIndex = 6;
             // 
             // btnStartGame
             // 
             this.btnStartGame.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnStartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStartGame.Location = new System.Drawing.Point(159, 192);
+            this.btnStartGame.Location = new System.Drawing.Point(212, 236);
+            this.btnStartGame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStartGame.Name = "btnStartGame";
-            this.btnStartGame.Size = new System.Drawing.Size(118, 37);
+            this.btnStartGame.Size = new System.Drawing.Size(157, 46);
             this.btnStartGame.TabIndex = 5;
             this.btnStartGame.Text = "Start Game";
             this.btnStartGame.UseVisualStyleBackColor = false;
@@ -158,43 +166,48 @@
             // 
             // txtName2
             // 
-            this.txtName2.Location = new System.Drawing.Point(117, 116);
+            this.txtName2.Location = new System.Drawing.Point(156, 143);
+            this.txtName2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName2.Name = "txtName2";
-            this.txtName2.Size = new System.Drawing.Size(132, 35);
+            this.txtName2.Size = new System.Drawing.Size(175, 37);
             this.txtName2.TabIndex = 4;
             // 
             // txtName1
             // 
-            this.txtName1.Location = new System.Drawing.Point(117, 66);
+            this.txtName1.Location = new System.Drawing.Point(156, 81);
+            this.txtName1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName1.Name = "txtName1";
-            this.txtName1.Size = new System.Drawing.Size(132, 35);
+            this.txtName1.Size = new System.Drawing.Size(175, 37);
             this.txtName1.TabIndex = 3;
             // 
             // lPlayer2
             // 
             this.lPlayer2.AutoSize = true;
-            this.lPlayer2.Location = new System.Drawing.Point(35, 119);
+            this.lPlayer2.Location = new System.Drawing.Point(47, 146);
+            this.lPlayer2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lPlayer2.Name = "lPlayer2";
-            this.lPlayer2.Size = new System.Drawing.Size(86, 29);
+            this.lPlayer2.Size = new System.Drawing.Size(130, 31);
             this.lPlayer2.TabIndex = 2;
             this.lPlayer2.Text = "Player 2:";
             // 
             // lPlayer1
             // 
             this.lPlayer1.AutoSize = true;
-            this.lPlayer1.Location = new System.Drawing.Point(35, 64);
+            this.lPlayer1.Location = new System.Drawing.Point(47, 79);
+            this.lPlayer1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lPlayer1.Name = "lPlayer1";
-            this.lPlayer1.Size = new System.Drawing.Size(82, 29);
+            this.lPlayer1.Size = new System.Drawing.Size(130, 31);
             this.lPlayer1.TabIndex = 1;
             this.lPlayer1.Text = "Player 1:";
             // 
             // lEnterName
             // 
             this.lEnterName.AutoSize = true;
-            this.lEnterName.Font = new System.Drawing.Font("Pristina", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lEnterName.Location = new System.Drawing.Point(35, 25);
+            this.lEnterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lEnterName.Location = new System.Drawing.Point(47, 31);
+            this.lEnterName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lEnterName.Name = "lEnterName";
-            this.lEnterName.Size = new System.Drawing.Size(214, 32);
+            this.lEnterName.Size = new System.Drawing.Size(358, 36);
             this.lEnterName.TabIndex = 0;
             this.lEnterName.Text = "Please enter your name:";
             // 
@@ -202,11 +215,12 @@
             // 
             this.btnExit.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.Color.Black;
-            this.btnExit.Location = new System.Drawing.Point(235, 357);
+            this.btnExit.Location = new System.Drawing.Point(313, 439);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(97, 28);
+            this.btnExit.Size = new System.Drawing.Size(129, 34);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Exit Game";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -214,11 +228,11 @@
             // 
             // MenuForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(356, 405);
+            this.ClientSize = new System.Drawing.Size(506, 498);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.panelPlayer);
             this.Controls.Add(this.btnHard);
@@ -227,6 +241,7 @@
             this.Controls.Add(this.btn1Player);
             this.Controls.Add(this.btnScores);
             this.Controls.Add(this.lConcentration);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MenuForm";
             this.Text = "Concentration";
             this.panelPlayer.ResumeLayout(false);
